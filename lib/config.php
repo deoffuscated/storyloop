aa6acfa0c431075dd370263a954a2def97de13ae
魏澤 <30227165+deoffuscated@users.noreply.github.com>
2023-05-27 08:39:29 +0700

    Add files via upload

---
+++ lib/config.php
@@ -0,0 +1,18 @@
+<?php
+include('func.php');
+include('igfunc.php');
+date_default_timezone_set('Asia/Jakarta');
+error_reporting(0);
+
+
+
+$countTarget    = '5'; 
+$sleep_1        = '120'; 
+$sleep_2        = '120';
+
+$saveFile 		= 'logData.txt'; 
+$cookieFile 	= 'cookieData.txt'; 
+$targetFile 	= 'targetData.txt'; // File target
+$date 			= date("Y-m-d");
+$time 			= date("H:i:s");
+?>
